dcd20832e664fba627ffa74fe708a4bd48a61195
Brent Hueth <hueth@wisc.edu>
2015-01-16 19:38:25 -0600

    Moved files back to where they were suppose to be.

--- code/land_allocation_risk.py
+++ code/land_allocation_risk.py
@@ -13,7 +13,7 @@ printing.init_printing()
 
 # <codecell>
 
-a,d,q,r,L,pi1,pi2,piH,piL,c = sp.symbols('alpha delta q rho L pi_1 pi_2 pi_H pi_L c')
+a, d, q, r, L, pi1, pi2, piH, piL,c = sp.symbols('alpha delta q rho L pi_1 pi_2 pi_H pi_L c')
 
 # <codecell>
 
@@ -27,19 +27,23 @@ u = lambda x: x**r
 u_a = lambda x: u(x*L*pi1 + (1-x)*L*pi2)
 
 
-# Note that I'm manually factoring L and then implicitly 
+# Note that I'm manually factoring L and then implicitly
 # setting the equation to zero and removing L (by setting it to 1).
 # Solving this equation is a little tricky.
-expected_marginal_util = (q*u_a(a).diff(a).factor(L).subs(pi2,piL)+(1-q)*u_a(a).diff(a).factor(L).subs(pi2,piH)).subs(L,1)
-D = ((1-q)*(piH-pi1)/(q*(pi1-piL)))**(1/(1-r))
-solution = (piH-D*piL)/(D*(pi1-piL)+(piH-pi1))
+
+marginal_utility_L = u_a(a).diff(a).factor(L).subs(pi2,piL)
+marginal_utility_H = u_a(a).diff(a).factor(L).subs(pi2,piH)
+expected_marginal_util = (q*marginal_utility_L + (1-q)*marginal_utility_H).subs(L,1)
+
+D = ((1 - q) * (piH - pi1)/(q * (pi1 - piL)))**(1 / (1 - r))
+solution = (piH - D * piL)/(D * (pi1 - piL) + (piH - pi1))
 
 # <codecell>
 
-parameters = {q: .3,pi1: 1,piL: 0,piH: 2}
+parameters = {q:.3, pi1:1, piL:0, piH:2}
 
 # Recall that low rho represents high risk aversion.
-sp.plot(solution.subs(parameters),(r,.01,.99))
+sp.plot(solution.subs(parameters), (r, .01, .99))
 
 # <codecell>
 
