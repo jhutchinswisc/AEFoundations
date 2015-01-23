d709739dff82ec2b72643b5f76546482c6e16bb7
jhutchinswisc <jhutchins@wisc.edu>
2015-01-23 15:32:57 -0600

    Added solutions with exercise1.py

---
+++ solutions/exercise1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import scipy as sp
+
+class LandAllocation(object):
+    """"Calculates the optimal land allocation
+    given probability distribution and model parameters"""
+    def __init__(self,land,crop1,crop2)
+	self.land  = land	# Input total land area
+	self.crop1 = crop1      # Input crop1 prob distribution
+	self.crop2 =  		# Input crop2 prob distribution
+    def utility(alpha,beta,pi)
+	
+	EU = alpha*self.expect(pi) +beta*self.var(pi)
+	return EU
+
+LandAllocation(Jared)
+Jared.land = 50
+Jared.crop1 =25
+Jared.crop2 =25
+Jared.utility(4,1,pi)
+	
